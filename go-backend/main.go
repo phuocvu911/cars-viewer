34ae40b0ffeda00a542e2b987199bd02c09bdc15
markusnieminen1 <65071702+markusnieminen1@users.noreply.github.com>
2026-05-28 23:36:09 +0300

    Reverse proxy and /car endpoint added

--- go-backend/main.go
+++ go-backend/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"cars-viewer/handlers"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /car/", handlers.CarDetailsHandler)
+
+	// Proxy image requests to localhost:3000
+	remoteURL, _ := url.Parse(handlers.API_BASE_URL)
+	proxy := httputil.NewSingleHostReverseProxy(remoteURL)
+
+	mux.HandleFunc("/api/images/", func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
+
+	http.ListenAndServe(":8080", mux)
+}
